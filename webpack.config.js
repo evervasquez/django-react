c190eec4e73bb350baf5c712743fb6538bbe3640
ever vasquez <ever@mobytes.pe>
2019-09-24 22:59:29 -0500

    add antd

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,29 @@
+const path = require('path');
+
 module.exports = {
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader"
-        }
-      }
-    ]
-  }
+    entry: {
+        index: path.resolve(__dirname, './negoapps/app/frontend/src/index.js'),
+        //contact: path.resolve(__dirname, 'src/js/contact.js')
+    },
+    output: {
+        path: path.resolve(__dirname, './negoapps/app/frontend/static/frontend'),
+        filename: 'main.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            }, {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            }
+        ]
+    },
+    resolve: {
+     extensions: ['.js']
+   }
 };
